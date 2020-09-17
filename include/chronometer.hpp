90e4a3bbe670d5a3c77c8e8b285436207581987c
cerclique <aurelien.vernizeau@gmail.com>
2020-09-17 23:10:21 +0200

    Adding implementation of chronometer as a single header

---
+++ include/chronometer.hpp
@@ -0,0 +1,28 @@
+#include <chrono>
+
+class Chronometer {
+public:
+  typedef std::chrono::steady_clock clock;
+  typedef std::chrono::nanoseconds nanoseconds;
+  typedef std::chrono::microseconds microseconds;
+  typedef std::chrono::milliseconds milliseconds;
+  typedef std::chrono::seconds seconds;
+
+  Chronometer() : origin(clock::now()) {};
+  Chronometer(const Chronometer&) = delete;
+  Chronometer(const Chronometer&&) = delete;
+
+  Chronometer& operator=(const Chronometer&) = delete;
+
+  void reset() noexcept {
+    this->origin = clock::now();
+  };
+
+  template<class T>
+  uint64_t get() const {
+    return std::chrono::duration_cast<T>(clock::now() - this->origin).count();
+  }
+
+private:
+  clock::time_point origin;
+};
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <thread>
+
+#include "chronometer.hpp"
+
+int main(int argc, char** argv) {
+  (void)argc;
+  (void)argv;
+
+  Chronometer timer;
+
+  for(uint32_t i = 0; i < 5; ++i) {
+    std::this_thread::sleep_for(Chronometer::seconds(1));
+  }
+
+  std::cout << timer.get<Chronometer::nanoseconds>() << " nanoseconds"<< std::endl;
+  std::cout << timer.get<Chronometer::microseconds>() << " microseconds"<< std::endl;
+  std::cout << timer.get<Chronometer::milliseconds>() << " milliseconds"<< std::endl;
+  std::cout << timer.get<Chronometer::seconds>() << " seconds"<< std::endl;
+
+  return 0;
+}
