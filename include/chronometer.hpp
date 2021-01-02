515712180d8f01f34a075482423133feba7e9de3
cerclique <aurelien.vernizeau@gmail.com>
2021-01-02 15:43:29 +0100

    Fix deleted constructor

--- include/chronometer.hpp
+++ include/chronometer.hpp
@@ -9,10 +9,11 @@ public:
   typedef std::chrono::seconds seconds;
 
   Chronometer() : origin(clock::now()) {};
-  Chronometer(const Chronometer&) = delete;
-  Chronometer(const Chronometer&&) = delete;
 
+  Chronometer(const Chronometer&) = delete;
+  Chronometer(Chronometer&&) = delete;
   Chronometer& operator=(const Chronometer&) = delete;
+  Chronometer& operator=(Chronometer&&) = delete;
 
   void reset() noexcept {
     this->origin = clock::now();
